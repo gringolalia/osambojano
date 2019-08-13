3f55f175fb6c819774ee4ddedaec67a00d9b074a
Colin Brayton <gringolalia@gmail.com>
2019-08-13 10:36:36 -0300

    Initial dist subtree commit

---
+++ dist/precache-manifest.09753b0e249eb7b0d9d0811143d03ef5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "21393b2c1bcf915887c5",
+    "url": "/js/about.6c97fb3b.js"
+  },
+  {
+    "revision": "8497be4753b83e1035a8",
+    "url": "/css/app.02685768.css"
+  },
+  {
+    "revision": "8497be4753b83e1035a8",
+    "url": "/js/app.c043cce0.js"
+  },
+  {
+    "revision": "4926e86e285f82d36fad",
+    "url": "/css/chunk-vendors.c1690696.css"
+  },
+  {
+    "revision": "4926e86e285f82d36fad",
+    "url": "/js/chunk-vendors.2dcaa7f7.js"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "42406d506a3d5ba3e5be54984d58ed4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
