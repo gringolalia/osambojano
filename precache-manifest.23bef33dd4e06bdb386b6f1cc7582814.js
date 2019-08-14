fd6f01c447a4dc198b3edbc317f133d812571b49
Colin Brayton <gringolalia@gmail.com>
2019-08-14 11:49:20 -0300

    deploy

---
+++ precache-manifest.23bef33dd4e06bdb386b6f1cc7582814.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "21393b2c1bcf915887c5",
+    "url": "/osambojano/js/about.6c97fb3b.js"
+  },
+  {
+    "revision": "18fc3b59505c4f6a3daa",
+    "url": "/osambojano/css/app.02685768.css"
+  },
+  {
+    "revision": "18fc3b59505c4f6a3daa",
+    "url": "/osambojano/js/app.989315ea.js"
+  },
+  {
+    "revision": "4926e86e285f82d36fad",
+    "url": "/osambojano/css/chunk-vendors.c1690696.css"
+  },
+  {
+    "revision": "4926e86e285f82d36fad",
+    "url": "/osambojano/js/chunk-vendors.2dcaa7f7.js"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/osambojano/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "1b07b1f9b0483b05f7c91c6de8feed88",
+    "url": "/osambojano/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/osambojano/robots.txt"
+  }
+];
